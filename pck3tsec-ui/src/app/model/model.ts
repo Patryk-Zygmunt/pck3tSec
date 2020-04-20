7be635be84bbfabd9cccbc2600875437ce20e001
Patryk-Zygmunt <patrykzygmuntzx@gmail.com>
2020-04-20 17:41:40 +0200

    model

---
+++ pck3tsec-ui/src/app/model/model.ts
@@ -0,0 +1,24 @@
+export interface HostList {
+  id: number;
+  host: string;
+  reason: string;
+  time: Date;
+}
+
+export interface Host {
+  id: number;
+  host: string;
+  blocked: boolean;
+  ip: string;
+  threat: boolean;
+}
+
+export interface Threat {
+  id: number;
+  host: string;
+  type: string;
+  details: string;
+  threat: boolean;
+  discovered: Date;
+
+}
