a30680d7d0e7b113d76ac98b378e4e8aa269c26a
Patryk-Zygmunt <patrykzygmuntzx@gmail.com>
2020-04-20 15:47:40 +0200

    init2 frontend

--- pck3tsec-ui/src/app/pages/pages-routing.module.ts
+++ pck3tsec-ui/src/app/pages/pages-routing.module.ts
@@ -5,11 +5,17 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ECommerceComponent } from './e-commerce/e-commerce.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+import {HostListComponent} from "./host-list/host-list.component";
 
 const routes: Routes = [{
   path: '',
   component: PagesComponent,
   children: [
+
+    {
+      path: 'd',
+      component: HostListComponent,
+    },
     {
       path: 'dashboard',
       component: ECommerceComponent,
