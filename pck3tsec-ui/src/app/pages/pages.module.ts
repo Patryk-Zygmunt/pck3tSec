13aeca65d2e4c3b773c5b4dca4db2fc898ed215a
Patryk-Zygmunt <patrykzygmuntzx@gmail.com>
2020-04-20 15:47:40 +0200

    init2 frontend

--- pck3tsec-ui/src/app/pages/pages.module.ts
+++ pck3tsec-ui/src/app/pages/pages.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule } from '@nebular/theme';
+import {NbCardModule, NbListModule, NbMenuModule} from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
@@ -7,18 +7,24 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import {HostListComponent} from "./host-list/host-list.component";
 
 @NgModule({
   imports: [
+    ThemeModule,
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
     DashboardModule,
     ECommerceModule,
     MiscellaneousModule,
+    NbListModule,
+    NbCardModule,
+
   ],
   declarations: [
     PagesComponent,
+    HostListComponent
   ],
 })
 export class PagesModule {
